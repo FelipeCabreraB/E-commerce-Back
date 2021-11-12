30d16d783673c1e76eb3f926c76e3fab10bbe033
ignacionin <ignaninm@gmail.com>
2021-11-12 15:46:35 -0300

    6 accesories

--- seeders/Seeder.js
+++ seeders/Seeder.js
@@ -20,13 +20,55 @@ module.exports = async () => {
   }
 
   //SEEDER DE ACCESSORIES
-  /*  const productName
- const description
- const characteristic1
- const characteristic2
- const characteristic3
- const price
- const picture */
+  const accProductName = [
+    "Aeropress Go",
+    "Aeropress",
+    "Hario grinder",
+    "V60 Hario",
+    "Kit V60 Hario",
+    "Swan neck pitcher",
+  ];
+  const accDescription = [
+    "The AeroPress is a combination of a French press or plunger coffee maker with a paper filter that uses air pressure to make coffee with more oils and less sediment in record time.",
+    "The AeroPress is a combination of a French press or plunger coffee maker with a paper filter that uses air pressure to make coffee with more oils and less sediment in record time.",
+    "The super compact ceramic Smart G manual grinder is perfect for home use. This Hario grinder has a clear body, allowing you to see the coffee beans. The handle slides sideways for easy storage. Compact design allows the handle to be stored within the band portion for convenient portability",
+    "The V60 is iconic and recognized around the world for its functionality and design. The conical shape of the V60 allows for a deeper layer of coffee, and its spiral ridges and lack of flow restriction allow the user to pour water quickly for a delicate body or slowly for a heavier flavor. The dropper features Hario's signature heat resistant glass.",
+    "V60 Ceramic Dripper 02 Set. The V60 ceramic dripper is manufactured using the traditional Arita yaki Method that has been passed down for 400 years. The traditional Japanese “Monozukuri” (innovative production) spirit that pays attention to small details supports the functional beauty of HARIO. Includes: Hario V60 Ceramic Dripper size 02. Hario V60 600ml Serving jug. Pack of 100 filters",
+    "Easy-to-use stainless steel jug. It offers a slim spout to facilitate the pouring of the water in the preparation of your coffee. Exclusive thin and elongated spout that will allow you to perfectly control the location and flow of water that is poured over the coffee. It is suitable for direct fire, glass ceramic and induction. It is specially designed to be used for infusers such as Aeropress, V60 and Chemex!",
+  ];
+  const accCharacteristic1 = [
+    "Capacity: 2 cups",
+    "Capacity: 4 cups",
+    "Capacity: 24 gr",
+    "Capacity: 1 to 2 cups",
+    "Hario V60 Ceramic Dripper size 02",
+    "Max capacity: 500 ml",
+  ];
+  const accCharacteristic2 = [
+    "Filter",
+    "Filter",
+    "Knob to adjust the grind",
+    "Heat glass resistant",
+    "Hario V60 600ml Serving jug",
+    "Stainless steel",
+  ];
+  const accCharacteristic3 = [
+    "Mixer",
+    "Mixer",
+    "Compact size",
+    "Mixer spoon",
+    "100 filters pack",
+    "Suitable for direct fire, glass ceramic and induction.",
+  ];
+  const accPrice = [2590, 2590, 4990, 2290, 3990, 4990];
+  const accPicture = [
+    "https://cultocafe.uy/wp-content/uploads/2021/08/Kiosco__0005_Layer-9.jpg",
+    "https://cultocafe.uy/wp-content/uploads/2021/08/Kiosco__0008_Layer-4.jpg",
+    "https://cultocafe.uy/wp-content/uploads/2021/08/Kiosco__0002_Layer-13.jpg",
+    "https://cultocafe.uy/wp-content/uploads/2021/08/Kiosco__0009_Layer-3.jpg",
+    "https://cultocafe.uy/wp-content/uploads/2021/08/Kiosco__0003_Layer-11.jpg",
+    "https://cultocafe.uy/wp-content/uploads/2021/08/Kiosco__0010_Layer-2.jpg",
+  ];
 
   //SEEDER DE PRODUCTS
   const productName = ["Timbo", "Nauyaca", "Women", "Sabanera", "Boa", "Mamba Negra"];
