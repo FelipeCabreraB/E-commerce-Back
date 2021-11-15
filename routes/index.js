0dfee3cd53b3b289e0f1e0846c0c9894c2baef2c
FelipeCabreraB <felipemartincabrera@gmail.com>
2021-11-15 09:27:59 -0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const publicRoutes = require("./publicRoutes");
 const adminRoutes = require("./adminRoutes");
 
 module.exports = (app) => {
-  app.use(publicRoutes);
-  app.use("/admin", adminRoutes);
+  app.use("/api", publicRoutes);
+  app.use("/api/admin", adminRoutes);
 };
