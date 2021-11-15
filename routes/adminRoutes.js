aa6fbcf04e0371d1b3f4fada715856e3c6b59e7e
ignacionin <ignaninm@gmail.com>
2021-11-15 15:19:02 -0300

    Admin Order y User

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,11 +2,16 @@ const express = require("express");
 const adminRouter = express.Router();
 const adminProductController = require("../controllers/adminProductController");
 const adminCategoryController = require("../controllers/adminCategoryController");
+const adminOrderController = require("../controllers/adminOrderController");
+const adminUserController = require("../controllers/adminUserController")
 
 // Rutas del Admin:
 // ...
 
 adminRouter.get("/products", adminProductController.index);
 adminRouter.get("/categories", adminCategoryController.index);
+adminRouter.get("/orders", adminOrderController.index);
+adminRouter.get("/users", adminUserController.index);
+
 
 module.exports = adminRouter;
