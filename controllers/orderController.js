0314968169ef1a7c53e99e52a1f38a1152d3a354
FelipeCabreraB <felipemartincabrera@gmail.com>
2021-11-18 13:36:10 -0300

    orders

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,4 +1,5 @@
 const { Order } = require("../models");
+const { Product_Order } = require("../models");
 
 // Display a listing of the resource.
 async function index(req, res) {
@@ -22,7 +23,34 @@ async function show(req, res) {
 async function create(req, res) {}
 
 // Store a newly created resource in storage.
-async function store(req, res) {}
+async function store(req, res) {
+  const cart = req.body.cart;
+  const userId = req.body.userId;
+  const order = { statusOrder: "Pending", userId: userId };
+
+  const createdOrder = await Order.create(order);
+
+  await Product_Order.create({
+    quantity: 1,
+    price: 10,
+    grindingType: "expresso",
+    orderId: createdOrder.id,
+    productId: 1,
+  });
+
+  /*   for (const item of cart) {
+    await Product_Order.create({
+      quantity: item.quantity,
+      price: item.price,
+      grindingType: item.grindingType,
+      orderId: createdOrder.id,
+      productId: item.id,
+    });
+  } */
+  /*   console.log("productOrder: ", productOrder);
+
+  await Product_Order.create(productOrder); */
+}
 
 // Show the form for editing the specified resource.
 async function edit(req, res) {}
--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -3,6 +3,7 @@ const publicRouter = express.Router();
 const userController = require("../controllers/userController");
 const authController = require("../controllers/authController");
 const productController = require("../controllers/productController");
+const orderController = require("../controllers/orderController");
 
 // Rutas del Públicas:
 // ...
@@ -12,7 +13,7 @@ const productController = require("../controllers/productController");
 publicRouter.post("/login", authController.login);
 publicRouter.post("/register", userController.create);
 
-//Products
+// Products
 
 publicRouter.get("/products/:categoryId", productController.index);
 publicRouter.get("/featured/products", productController.indexFeatured);
@@ -20,5 +21,8 @@ publicRouter.get("/product/:productName", productController.show);
 
 publicRouter.get("/orders/:orderId", productController.show);
 
+// Orders
+
+publicRouter.post("/orders", orderController.store);
 
 module.exports = publicRouter;
